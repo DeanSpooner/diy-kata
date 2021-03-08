e9e977c4eaf5efe42b228c0572d03069bb949856
Dean Spooner <dean.spooner@me.com>
2021-03-08 22:38:47 +0000

    Completed making kata tests for 1 and 2.

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,18 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+    const canDivideBy3 = (number % 3) === 0;
+    const canDivideBy5 = (number % 5) === 0;
+    const canDivideBy15 = (number % 15) === 0;
+
+    if (canDivideBy15) {
+        return 'FizzBuzz';
+    }
+    if (canDivideBy5) {
+        return 'Buzz';
+    }
+    if (canDivideBy3) {
+        return 'Fizz';
+    }
+    return number;
+};
 
 module.exports = fizzBuzz;
--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,15 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+ it("returns 'Yes' when a boolean passes as true", () => {
+    expect(booleanToWord(true)).toBe("Yes");
+    expect(booleanToWord(2 * 2 === 4)).toBe("Yes");
+    expect(booleanToWord(9 < 10)).toBe("Yes");
+  });
+
+  it("returns 'No' when a boolean passes as false", () => {
+    expect(booleanToWord(false)).toBe("No");
+    expect(booleanToWord(2 * 2 === 5)).toBe("No");
+    expect(booleanToWord(9 > 10)).toBe("No");
+  });
 });
