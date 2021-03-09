79f968e941325aac6a98d99cfd68ab1735e15a9b
Dean Spooner <dean.spooner@me.com>
2021-03-09 21:51:00 +0000

    Completed kata test 5.

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,14 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    let time = (distance/speed);
+    let timeRounded = (Math.round(time * 2))/2;
+
+
+if (timeRounded === 1) {
+    return "I should be there in 1 hour."
+}
+
+return "I should be there in " + timeRounded + " hours."
+
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,15 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
+    expect(reachDestination(59, 6)).toBe("I should be there in 10 hours.");
+    expect(reachDestination(23, 40)).toBe("I should be there in 0.5 hours.");
+  });
+
+  it("returns string with estimated time of arrival, using 'hour' for one hour", () => {
+    expect(reachDestination(15, 16)).toBe("I should be there in 1 hour.");
+    expect(reachDestination(22, 24)).toBe("I should be there in 1 hour.");
+    expect(reachDestination(85, 81)).toBe("I should be there in 1 hour.");
+  });
 });
